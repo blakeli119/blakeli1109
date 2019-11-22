394b313e0a98c6128290bb85eef828c04e89c58a
blakeli119 <55729228+blakeli119@users.noreply.github.com>
2019-11-22 16:05:36 +0800

    Create binary_search_tree_06170222.py

---
+++ HW3/binary_search_tree_06170222.py
@@ -0,0 +1,15 @@
+class TreeNode(object):
+    def __init__(self,x):
+        self.val = x
+        self.left = None
+        self.right = None
+        
+class Solution(object):
+    def insert(self, root, val):
+        pass
+    def delete(self, root, target):
+        pass
+    def search(self, root, target):
+        pass
+    def modify(self, root, target, new_val):
+        pass
