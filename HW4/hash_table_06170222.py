b8170252f6feb9af8c37bf835fd9862ac077bbe9
blakeli119 <55729228+blakeli119@users.noreply.github.com>
2019-12-06 16:12:05 +0800

    Create hash_table_06170222.py

---
+++ HW4/hash_table_06170222.py
@@ -0,0 +1,17 @@
+from Crypto.Hash import MD5
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+      
+class MyHashSet:
+    def __init__(self, capacity=5):
+        self.capacity = capacity
+        self.data = [None] * capacity
+       
+    def add(self, key):
+        return pass
+    def remove(self, key):
+        return pass
+    def contains(self, key):
+        return pass
